6bb0f165876d562e82cea6c2d65e7a6aa56203ee
Claudia <cfierro@csumb.edu>
2025-10-13 18:40:23 -0700

    added drug price to drug table to account for individual drug prices. Table updates correctly. Need to show correct display on localhost

--- lab19.sql
+++ lab19.sql
@@ -1,59 +1,98 @@
 CREATE DATABASE IF NOT EXISTS prescription;
 USE prescription;
 
-CREATE TABLE doctor (
-    id INT NOT NULL AUTO_INCREMENT,
-    ssn VARCHAR(9) NOT NULL UNIQUE,
-    last_name VARCHAR(30) NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    specialty VARCHAR(30),
+CREATE TABLE doctor
+(
+    id             INT         NOT NULL AUTO_INCREMENT,
+    ssn            VARCHAR(9)  NOT NULL UNIQUE,
+    last_name      VARCHAR(30) NOT NULL,
+    first_name     VARCHAR(30) NOT NULL,
+    specialty      VARCHAR(30),
     practice_since INT,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE patient (
-    id INT NOT NULL AUTO_INCREMENT,
-    ssn VARCHAR(9) NOT NULL UNIQUE,
-    first_name VARCHAR(45) NOT NULL,
-    last_name VARCHAR(45) NOT NULL,
-    birthdate DATE NOT NULL,
-    street VARCHAR(100) NOT NULL,
-    city VARCHAR(45) NOT NULL,
-    state CHAR(2) NOT NULL,
-    zipcode CHAR(5) NOT NULL,
-    doctor_id INT NULL,
+CREATE TABLE patient
+(
+    id         INT          NOT NULL AUTO_INCREMENT,
+    ssn        VARCHAR(9)   NOT NULL UNIQUE,
+    first_name VARCHAR(45)  NOT NULL,
+    last_name  VARCHAR(45)  NOT NULL,
+    birthdate  DATE         NOT NULL,
+    street     VARCHAR(100) NOT NULL,
+    city       VARCHAR(45)  NOT NULL,
+    state      CHAR(2)      NOT NULL,
+    zipcode    CHAR(5)      NOT NULL,
+    doctor_id  INT          NULL,
     PRIMARY KEY (id),
     KEY idx_patient_last_name (last_name),
     CONSTRAINT fk_patient_doctor1
-    FOREIGN KEY (doctor_id) REFERENCES doctor(id)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
+        FOREIGN KEY (doctor_id) REFERENCES doctor (id)
+            ON DELETE SET NULL
+            ON UPDATE CASCADE
 );
 
-CREATE TABLE drug (
-    DrugID INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE drug
+(
+    DrugID   INT         NOT NULL AUTO_INCREMENT,
     DrugName VARCHAR(45) NOT NULL,
+    IdvDrugPrice DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (DrugID),
     UNIQUE KEY uq_drug_drugname (DrugName)
 );
 
-CREATE TABLE prescription (
-    RXID INT NOT NULL AUTO_INCREMENT,
-    Doctor_ID INT NOT NULL,
-    Patient_ID INT NOT NULL,
-    Drug_DrugID INT NOT NULL,
-    Quantity INT NOT NULL,
+CREATE TABLE prescription
+(
+    RXID         INT NOT NULL AUTO_INCREMENT,
+    Doctor_ID    INT NOT NULL,
+    Patient_ID   INT NOT NULL,
+    Drug_DrugID  INT NOT NULL,
+    Quantity     INT NOT NULL,
     NumOfRefills INT NOT NULL,
     PRIMARY KEY (RXID),
     CONSTRAINT fk_prescription_doctor1
-    FOREIGN KEY (Doctor_ID)   REFERENCES doctor(id)
-    ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (Doctor_ID) REFERENCES doctor (id)
+            ON DELETE CASCADE ON UPDATE CASCADE,
 
     CONSTRAINT fk_prescription_patient1
-    FOREIGN KEY (Patient_ID)  REFERENCES patient(id)
-    ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (Patient_ID) REFERENCES patient (id)
+            ON DELETE CASCADE ON UPDATE CASCADE,
 
     CONSTRAINT fk_prescription_drug1
-    FOREIGN KEY (Drug_DrugID) REFERENCES drug(DrugID)
-    ON DELETE CASCADE ON UPDATE CASCADE
+        FOREIGN KEY (Drug_DrugID) REFERENCES drug (DrugID)
+            ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE pharmacy
+(
+    pharmacy_id INT         NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(45) NOT NULL,
+    address     VARCHAR(45) NOT NULL,
+    phone       VARCHAR(20) NOT NULL,
+    PRIMARY KEY (pharmacy_id)
+);
+
+CREATE TABLE prescription_fill
+(
+    fill_id     INT            NOT NULL AUTO_INCREMENT,
+    rxid        INT            NOT NULL,
+    pharmacy_id INT            NOT NULL,
+    date_filled DATE           NOT NULL,
+    price       DECIMAL(10, 2) NOT NULL,
+
+    PRIMARY KEY (fill_id),
+    CONSTRAINT fk_prescription
+        FOREIGN KEY (rxid)
+            REFERENCES prescription (rxid)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_pharmacy
+        FOREIGN KEY (pharmacy_id)
+            REFERENCES pharmacy (pharmacy_id)
+            ON DELETE SET NULL,
+    CONSTRAINT fk_drug_price
+        FOREIGN KEY (price)
+            REFERENCES drug (IdvDrugPrice)
+            ON DELETE SET NULL
+
 );
--- src/main/java/application/ControllerPrescriptionFill.java
+++ src/main/java/application/ControllerPrescriptionFill.java
@@ -1,7 +1,8 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -15,60 +16,200 @@ import view.*;
 @Controller
 public class ControllerPrescriptionFill {
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-	/*
-	 * Patient requests form to fill prescription.
-	 */
-	@GetMapping("/prescription/fill")
-	public String getfillForm(Model model) {
-		model.addAttribute("prescription", new PrescriptionView());
-		return "prescription_fill";
-	}
+    /*
+     * Patient requests form to fill prescription.
+     */
+    @GetMapping("/prescription/fill")
+    public String getfillForm(Model model) {
+        model.addAttribute("prescription", new PrescriptionView());
+        return "prescription_fill";
+    }
 
-	// process data from prescription_fill form
-	@PostMapping("/prescription/fill")
-	public String processFillForm(PrescriptionView p, Model model) {
-
-		/*
-		 * valid pharmacy name and address, get pharmacy id and phone
-		 */
-		// TODO 
-		
-		// TODO find the patient information 
-
-		// TODO find the prescription 
+    // process data from prescription_fill form
+    @PostMapping("/prescription/fill")
+    public String processFillForm(PrescriptionView p, Model model) {
+        int pharmacyId = 0;
+        int quantity = 0;
+        int numRefills = 0;
+        int patientId = 0;
+        int drugId = 0;
+        int fillCount = 0;
+        BigDecimal totalPrice = BigDecimal.valueOf(0.0);
 
 
-		/*
-		 * have we exceeded the number of allowed refills
-		 * the first fill is not considered a refill.
-		 */
-		
-		// TODO 
-		
-		/*
-		 * get doctor information 
-		 */
-		// TODO
+        /*
+         * validate pharmacy name and address, get pharmacy id and phone
+         */
+        try (Connection con = getConnection();) {
+            PreparedStatement ps = con.prepareStatement("select pharmacy_id, phone from pharmacy where name = ? and address = ?");
+            ps.setString(1, p.getPharmacyName());
+            ps.setString(2, p.getPharmacyAddress());
 
-		/*
-		 * calculate cost of prescription
-		 */
-		// TODO 
-		
-		// TODO save updated prescription 
+            ResultSet rs = ps.executeQuery();
 
-		// show the updated prescription with the most recent fill information
-		model.addAttribute("message", "Prescription filled.");
-		model.addAttribute("prescription", p);
-		return "prescription_show";
-	}
-	
-	private Connection getConnection() throws SQLException {
-		Connection conn = jdbcTemplate.getDataSource().getConnection();
-		return conn;
-	}
+            if (!rs.next()) {
+                System.out.println(p.getPharmacyName() + " does not exist.");
+                model.addAttribute("message", "Pharmacy not found");
+                model.addAttribute("prescription", p);
+                return "prescription_fill";
+            }
+            pharmacyId = rs.getInt("pharmacy_id");
+            p.setPharmacyID(pharmacyId);
+            p.setPharmacyPhone(p.getPharmacyPhone());
+            p.setPharmacyAddress(p.getPharmacyAddress());
+
+        } catch (SQLException e) {
+            System.err.println("Database error: " + e.getMessage());
+            model.addAttribute("message", "Error accessing pharmacy data");
+            model.addAttribute("prescription", p);
+            return "prescription_fill";
+
+        }
+
+        /*
+         * validate rxid and patient last name matches name on prescription
+         */
+        try (Connection con = getConnection();) {
+            PreparedStatement ps = con.prepareStatement(
+                    "select pr.RXID, pt.last_name, pr.Quantity, pr.NumOfRefills, pr.Patient_ID, pr.Drug_DrugID " +
+                            "from prescription pr " +
+                            "join patient pt on pr.Patient_ID = pt.id " +
+                            "where pr.RXID = ?"
+            );
+            ps.setInt(1, p.getRxid());
+            ResultSet rs = ps.executeQuery();
+
+            if (!rs.next()) {
+                model.addAttribute("message", "Rxid not found");
+                model.addAttribute("prescription", p);
+                return "prescription_fill";
+            }
+
+            String ptLastName = rs.getString("last_name");
+            if (!p.getPatientLastName().equalsIgnoreCase(ptLastName)) {
+                model.addAttribute("message", "Patient last name does not match");
+                model.addAttribute("prescription", p);
+                return "prescription_fill";
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Database error: " + e.getMessage());
+            model.addAttribute("message", "Error validating prescription");
+            model.addAttribute("prescription", p);
+            return "prescription_fill";
+        }
+
+
+        /*
+         * have we exceeded the number of allowed refills
+         * the first fill is not considered a refill.
+         */
+
+        try (Connection con = getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "select count(*) as fill_count from prescription_fill where rxid = ?"
+            );
+            ps.setInt(1, p.getRxid());
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                fillCount = rs.getInt("fill_count");
+
+                if (fillCount >= numRefills + 1) {
+                    model.addAttribute("message", "Refill limit exceeded");
+                    model.addAttribute("prescription", p);
+                    return "prescription_fill";
+                }
+                p.setRefillsRemaining((numRefills + 1) - fillCount);
+            }
+        } catch (SQLException e) {
+            model.addAttribute("message", "Error checking refill count: " + e.getMessage());
+            model.addAttribute("prescription", p);
+            return "prescription_fill";
+        }
+
+        /*
+         * get doctor information
+         */
+        try (Connection con = getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "select d.id from doctor d " +
+                            "join prescription p on d.id = p.Doctor_ID " +
+                            "where rxid = ?"
+            );
+            ps.setInt(1, p.getRxid());
+            ResultSet rs = ps.executeQuery();
+
+            if (!rs.next()) {
+                model.addAttribute("message", "Doctor information not found");
+                model.addAttribute("prescription", p);
+                return "prescription_fill";
+            }
+
+        } catch (SQLException e) {
+            model.addAttribute("message", "Error verifying refill: " + e.getMessage());
+            model.addAttribute("prescription", p);
+            return "prescription_fill";
+        }
+
+        /*
+         * calculate cost of prescription
+         */
+        try (Connection con = getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "select d.IndvDrugPrice * p.Quantity as total_price " +
+                            "from drug d " +
+                            "join prescription p on d.DrugID = p.Drug_DrugID " +
+                            "where p.rxid = ?"
+            );
+            ps.setInt(1, p.getRxid());
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                totalPrice = rs.getBigDecimal("total_price");
+                model.addAttribute("totalPrice", totalPrice);
+                p.setCost(totalPrice.toString());
+            }
+            else {
+                model.addAttribute("message", "Unable to calculate price — prescription not found.");
+                model.addAttribute("prescription", p);
+                return "prescription_fill";
+            }
+
+        } catch (SQLException e) {
+            model.addAttribute("message", "Error verifying price: " + e.getMessage());
+            model.addAttribute("prescription", p);
+            return "prescription_fill";
+        }
+
+        // save updated prescription
+        try (Connection con = getConnection();) {
+            PreparedStatement ps = con.prepareStatement(
+                    "insert into prescription_fill (RXID, pharmacy_id, date_filled, price) VALUES (?, ?, CURRENT_DATE, ?)"
+            );
+            ps.setInt(1, p.getRxid());
+            ps.setInt(2, pharmacyId);
+            ps.setBigDecimal(3, totalPrice);
+            ps.executeUpdate();
+            p.setDateFilled(p.getDateFilled());
+        } catch (SQLException e) {
+            model.addAttribute("message", "Error updating prescription: " + e.getMessage());
+            model.addAttribute("prescription", p);
+            return "prescription_fill";
+        }
+
+        // show the updated prescription with the most recent fill information
+        model.addAttribute("message", "Prescription filled.");
+        model.addAttribute("prescription", p);
+        return "prescription_show";
+    }
+
+    private Connection getConnection() throws SQLException {
+        Connection conn = jdbcTemplate.getDataSource().getConnection();
+        return conn;
+    }
 
 }
